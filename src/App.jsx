cfe9767466aa922cc62fb909634084a5ed37b3c7
CristopherBarbosa <cristopherbarbosa07@gmail.com>
2024-09-10 20:17:40 -0300

    GlobalStyles

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Title } from '../styles'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        
+        <Title>Ok React</Title>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Title = styled.h2`
+    color: blue;
+    font-size: 30px;
+    border: 2px solid red;
+`
