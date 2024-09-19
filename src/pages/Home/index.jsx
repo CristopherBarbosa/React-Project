c0f5c324929b0ef1deaaa71f914fc70b617a2e1c
CristopherBarbosa <cristopherbarbosa07@gmail.com>
2024-09-18 21:24:21 -0300

    finished-styles

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import { Button, ContainerInputs, Form, Input, InputLabel, Title, TopBackground, Container } from './styles'
+import UserImage from '../../assets/users.png'
+import trash from '../../assets/trash.svg'
+
+import { useRef } from 'react'
+
+function Home() {
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  function registerNewUser(){
+
+  }
+
+  return (
+    <>
+      <Container>
+        <TopBackground>
+          <img src= {UserImage} alt="" />
+        </TopBackground>
+
+
+        <Form>
+          <Title>Cadastrar Usuário</Title>
+
+          <ContainerInputs>
+
+                <div>
+                  <InputLabel>Nome<span> *</span></InputLabel>
+                  <Input  type='text' placeholder='Nome do usuário' ref={inputName}/>
+                </div>
+                <div>
+                  <InputLabel>Idade<span> *</span></InputLabel>
+                  <Input type='number' placeholder='Idade do usuário' ref={inputAge}/>
+                </div>
+
+
+          </ContainerInputs>
+
+              <div style={{width:'100%'}}>
+                <InputLabel>E-mail<span> *</span></InputLabel>
+                <Input type='email' placeholder='E-mail do usuário' ref={inputEmail}/>
+              </div>
+
+          <Button type='button' onClick={registerNewUser}>Cadastrar Usuário</Button>
+        </Form>
+      </Container>
+
+    </>
+  )
+}
+
+export default Home
