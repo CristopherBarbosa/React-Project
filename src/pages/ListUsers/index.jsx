1c0059b2fc9293e3c836a8106b95dddf21b29c82
CristopherBarbosa <cristopherbarbosa07@gmail.com>
2024-10-11 20:00:55 -0300

    useEffect

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,25 +1,28 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "../../components/Button/styles"
 import TopBackground from "../../components/TopBackground"
 import api from "../../services/api"
 
 
 
+
 function ListUsers() {
+
 
     useEffect(() => {
         async function getUsers(){
             const usersFromApi = await api.get('/usuarios')
+            console.log(usersFromApi)
         }
         getUsers()
     }, [])
     
-
     return (
         <div>
             
             <TopBackground />
             <h1>Listagem de Usuários</h1>
+
             <Button>Voltar</Button>
 
 
